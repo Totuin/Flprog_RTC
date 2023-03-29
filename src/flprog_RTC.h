33b4fc447a9f0dc5f369c3bcefc0d3e9c7d56bb3
Totuin <117037443+Totuin@users.noreply.github.com>
2023-03-29 20:52:27 +0400

    Update flprog_RTC.h

--- src/flprog_RTC.h
+++ src/flprog_RTC.h
@@ -8,16 +8,16 @@
 #endif
 
 #include "flprogUtilites.h"
-#include "base\flprog_RTC_base.h"
+#include "base/flprog_RTC_base.h"
 
 #if defined(STM32_SYSTEM_RTC)
 #include "flprogSTM32RTC.h"
 #endif
 
 #if defined(SOFTWARE_SYSTEM_RTC)
-#include "software\flprogAvrSoftRTC.h"
+#include "software/flprogAvrSoftRTC.h"
 #endif
 
-#include "i2c\flprog_i2c_RTC.h"
+#include "i2c/flprog_i2c_RTC.h"
 
-#include "ds1302\flprog_ds1302.h"
+#include "ds1302/flprog_ds1302.h"
